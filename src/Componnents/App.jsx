e348000db7de69a610f74dc8085b7a18e23085dc
unknown <aitzennisalaheddine1@gmail.com>
2024-09-29 23:18:12 +0100

    Removed Initial items (was for test)

--- src/Componnents/App.jsx
+++ src/Componnents/App.jsx
@@ -4,13 +4,8 @@ import Stats from "./Stats";
 import PackingList from "./PackingList";
 import Form from "./Form";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   const handleAddItems = function (newItem) {
     setItems((items) => [...items, newItem]);
